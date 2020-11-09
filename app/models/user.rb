376b874e52ec13828a12c19c05aced07253ce096
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2020-11-09 14:50:07 +1100

    Make addresses optional for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  belongs_to :address, dependent: :destroy
+  belongs_to :address, dependent: :destroy, optional: true
   has_many :orders
   has_many :listings
 end
