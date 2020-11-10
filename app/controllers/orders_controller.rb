6f25a3b1189aea0cabec6e7e411b170e85b89145
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2020-11-10 12:58:33 +1100

    Added elements for stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,31 @@ class OrdersController < ApplicationController
     def index
         @orders = Order.all
     end
+
+    def show
+        session = Stripe::CheckoutSession.create(
+            payment_method_types: '[card]',
+            customer_email: current_user.email,
+            line_items: [(
+                name: @listing.title,
+                description: @listing.description,
+                image: @listing.picture,
+                amount: (@listing.price * 100).to_i,
+                currency: 'AUD',
+                quantity: 1
+            )],
+            payment_intent_data: {
+                metadata: {
+                    listing_id: @listing.id
+                }
+            },
+            success_url: "#{root_url}payments/success?listingId=#{@listing.id}",
+            cancel_url: "#{root_url}events"
+        )
+        @session_id = @session.id
+    end
+
+    def success
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   resources :listings
+
   resources :orders
+  get "/orders/success", to: "orders#success"
+
+
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
   root to: "listings#index"
 end
