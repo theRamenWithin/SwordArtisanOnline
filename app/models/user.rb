21047a3887c47059e8b5a2fc56d11b1d2a803bfb
Alex Pike <55725286+theRamenWithin@users.noreply.github.com>
2020-11-14 11:48:15 +1100

    Twitter auth fills first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,8 @@ class User < ApplicationRecord
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
     user.email = auth.info.email
     user.password = Devise.friendly_token[0, 20]
-    user.first_name = auth.info.name
+    user.first_name = auth.info.name.split[0]
+    user.last_name = auth.info.name.split[1..].join(' ')
     user.username = auth.info.nickname
     #user.image = auth.info.image # assuming the user model has an image
     # user.skip_confirmation!
